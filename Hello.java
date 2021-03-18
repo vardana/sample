1213e30f07655693fd193e7d38f15dc2d1f4bd14
vardana <aivazyan@gmail.com>
2021-03-18 17:01:51 +0400

    Counter up to 35

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args){
-        for(int i=1;i<=30;i++){
+        for(int i=1;i<=35;i++){
             System.out.println("Hello World ... "+i);
         }
     }
